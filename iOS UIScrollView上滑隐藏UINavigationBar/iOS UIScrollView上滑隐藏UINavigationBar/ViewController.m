99b48c533f196f6cfe62acbb16b7c285e00076f7
yangqian111 <787688073@qq.com>
2016-10-31 21:33:09 +0800

    提交 关于偏移的说明

--- iOS UIScrollView上滑隐藏UINavigationBar/iOS UIScrollView上滑隐藏UINavigationBar/ViewController.m
+++ iOS UIScrollView上滑隐藏UINavigationBar/iOS UIScrollView上滑隐藏UINavigationBar/ViewController.m
@@ -44,6 +44,10 @@
 }
 
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    //这里做一下说明  因为在iOS7之后  如果UIScrolView是第一个被 [self.view addSubview:scrollView] 添加到
+    //当前的VC view上   那么之后添加到UIScrollView上的view都会在Y方向上 向下偏移64 这里加上64就是为了让UIScrollview的偏移量处于0  这样offsetY就是我们真正手势滑动的距离  如果不想要UIScrollView偏移，那么需要在VC上使用如下方法：
+    //self.automaticallyAdjustsScrollViewInsets = NO 如果这样用  那么就需要在滑动时  改变TableView的frame 这样  我们移动NavigationBar的时候 才不会出现空白view
     CGFloat offsetY = scrollView.contentOffset.y+64;
     if (offsetY > 0) {
         if (offsetY >= 44) {
